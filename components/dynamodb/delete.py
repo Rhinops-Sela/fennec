b68186a5b7ded75e9e48c10b7c4051f08a83088f
Ilia <iliag@sela.co.il>
2021-10-10 20:07:57 +0300

    fixed dynamodb delete

--- components/dynamodb/delete.py
+++ components/dynamodb/delete.py
@@ -4,10 +4,12 @@ from fennec_executers.helm_executer import Helm
 from fennec_execution.execution import Execution
 
 execution = Execution(os.path.dirname(__file__))
-sqs_url = execution.local_parameters['SQS_DNS_RECORD']
+dynamodb_url = execution.local_parameters['DYNAMO_DNS_RECORD']
+dynamodb_admin_url = execution.local_parameters['DYNAMO_ADMIN_DNS_RECORD']
 namespace = execution.local_parameters['NAMESPACE']
-helm_chart = Helm(os.path.dirname(__file__), namespace=namespace, chart_name="sqs")
+helm_chart = Helm(os.path.dirname(__file__), namespace=namespace, chart_name="dynamodb")
 helm_chart.uninstall_chart()
 core_dns = CoreDNS(os.path.dirname(__file__))
-core_dns.delete_records(sqs_url)
+core_dns.delete_records(dynamodb_url)
+core_dns.delete_records(dynamodb_admin_url)
 #helm_chart.delete_namespace(namespace)
