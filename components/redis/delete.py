ba36fbcfc46fca1366e0a51bfee5ae79dc1e0596
Ilia <iliag@sela.co.il>
2021-10-10 19:15:23 +0300

    added dns record delete to redis + sqs

--- components/redis/delete.py
+++ components/redis/delete.py
@@ -9,4 +9,6 @@ redis_admin_url = execution.local_parameters['REDIS_ADMIN_DNS_RECORD']
 namespace = execution.local_parameters['NAMESPACE']
 helm_chart = Helm(os.path.dirname(__file__), namespace=namespace, chart_name="redis")
 helm_chart.uninstall_chart()
-#helm_chart.delete_namespace(namespace)
+core_dns = CoreDNS(os.path.dirname(__file__))
+core_dns.delete_records(redis_url)
+core_dns.delete_records(redis_admin_url)
--- components/sqs/delete.py
+++ components/sqs/delete.py
@@ -4,8 +4,10 @@ from fennec_executers.helm_executer import Helm
 from fennec_execution.execution import Execution
 
 execution = Execution(os.path.dirname(__file__))
-redis_url = execution.local_parameters['SQS_DNS_RECORD']
+sqs_url = execution.local_parameters['SQS_DNS_RECORD']
 namespace = execution.local_parameters['NAMESPACE']
 helm_chart = Helm(os.path.dirname(__file__), namespace=namespace, chart_name="sqs")
 helm_chart.uninstall_chart()
+core_dns = CoreDNS(os.path.dirname(__file__))
+core_dns.delete_records(sqs_url)
 #helm_chart.delete_namespace(namespace)
