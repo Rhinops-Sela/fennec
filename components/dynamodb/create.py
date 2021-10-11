8a6075dbf4e5ecf9bafa3ac024b8ff2095e7d4c6
Ilia <iliag@sela.co.il>
2021-10-11 13:51:46 +0300

    dynamo db fox admin ui

--- components/dynamodb/create.py
+++ components/dynamodb/create.py
@@ -16,7 +16,8 @@ template_path = os.path.join(
 nodegroup = Nodegroup(os.path.dirname(__file__), template_path)
 nodegroup.create()
 
-helm_chart = Helm(os.path.dirname(__file__), namespace=namespace, chart_name="localstack")
+helm_chart = Helm(os.path.dirname(__file__),
+                  namespace=namespace, chart_name="localstack")
 values_file_path = os.path.join(
     execution.execution_folder, "values.json")
 
@@ -24,17 +25,28 @@ values_file_object = Helper.file_to_object(values_file_path)
 values_file_object['extraEnvVars'][0]['value'] = helm_chart.execution.cluster_region
 execution_file = os.path.join(
     os.path.dirname(__file__), "dynamodb-execute.values.json")
-Helper.to_json_file(values_file_object, execution_file)    
+Helper.to_json_file(values_file_object, execution_file)
 
 helm_chart.install_chart(release_name="localstack-charts",
-                                  chart_url="https://localstack.github.io/helm-charts",
-                                  deployment_name="dynamodb",
-                                  additional_values=[f"--values {execution_file}"])
-dynamodb_record = f"{dynamodb_url}=dynamodb-localstack.{namespace}.svc.cluster.local"                                 
+                         chart_url="https://localstack.github.io/helm-charts",
+                         deployment_name="dynamodb",
+                         additional_values=[f"--values {execution_file}"])
+dynamodb_record = f"{dynamodb_url}=dynamodb-localstack.{namespace}.svc.cluster.local"
 
 kubectl = Kubectl(os.path.dirname(__file__))
-kubectl.install_folder("admin", namespace=nodegroup.execution.get_local_parameter('NAMESPACE'))
-admin_record  = f"{dynamodb_admin_url}=dynamodb-local-admin.{nodegroup.execution.get_local_parameter('NAMESPACE')}.svc.cluster.local"
+values_to_replace = {
+    'AWS_REGION': f'{kubectl.execution.cluster_region}'}
+ui_deployment_template = os.path.join(
+    kubectl.execution.templates_folder, "admin", "01.deployment.json")
+ui_deployment_template_output = os.path.join(
+    kubectl.execution.templates_folder, "admin", "01.deployment-execute.json")
+Helper.replace_in_file(
+    ui_deployment_template, ui_deployment_template_output, values_to_replace)
+
+kubectl.install_file(
+    os.path.join(execution.templates_folder, "admin", '02.service.json'), namespace=nodegroup.execution.get_local_parameter('NAMESPACE'))
+
+admin_record = f"{dynamodb_admin_url}=dynamodb-local-admin.{nodegroup.execution.get_local_parameter('NAMESPACE')}.svc.cluster.local"
 
 
 core_dns = CoreDNS(os.path.dirname(__file__))
