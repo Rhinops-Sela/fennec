8c9ecb53c11c66616c2791cccc6219eac6bf6f30
shaharblanksela <91251849+shaharblanksela@users.noreply.github.com>
2021-10-17 14:56:21 +0300

    Update script.sh

--- devops/metadata/script.sh
+++ devops/metadata/script.sh
@@ -3,8 +3,10 @@
 sed -i "/backendUrl/c\  \"backendUrl\": \"$BACKEND_URL\"," /usr/share/nginx/html/assets/config.json
 sed -i "/socketUrl/c\  \"socketUrl\": \"$SOCKET_URL\"," /usr/share/nginx/html/assets/config.json
 
-cd /app
-echo "cat script.sh"
-cat script.sh
+echo "cat config.json"
+cat /usr/share/nginx/html/assets/config.json
+
+nginx stop
+nginx start
 
 pm2 start /app/backend/dist/src/index.js && nginx -g 'daemon off;'
