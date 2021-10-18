be4ccc8819d15613c5f81921e3993d8b4ed8cd9e
shahar blank <shaharb@sela.co.il>
2021-10-18 14:28:57 +0300

    commented sed command - test

--- devops/metadata/script.sh
+++ devops/metadata/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i "/backendUrl/c\  \"backendUrl\": \"$BACKEND_URL\"," /usr/share/nginx/html/assets/config.json
-sed -i "/socketUrl/c\  \"socketUrl\": \"$SOCKET_URL\"," /usr/share/nginx/html/assets/config.json
+# sed -i "/backendUrl/c\  \"backendUrl\": \"$BACKEND_URL\"," /usr/share/nginx/html/assets/config.json
+# sed -i "/socketUrl/c\  \"socketUrl\": \"$SOCKET_URL\"," /usr/share/nginx/html/assets/config.json
 
 echo "cat config.json"
 cat /usr/share/nginx/html/assets/config.json
