ebb543b9f881b72cec70612116b8a0cad4004996
shahar blank <shaharb@sela.co.il>
2021-10-18 16:19:23 +0300

    change sed command

--- devops/metadata/script.sh
+++ devops/metadata/script.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# sed -i "/backendUrl/c\  \"backendUrl\": \"$BACKEND_URL\"," /usr/share/nginx/html/assets/config.json
-# sed -i "/socketUrl/c\  \"socketUrl\": \"$SOCKET_URL\"," /usr/share/nginx/html/assets/config.json
+sed -i "s|localhost|$BACKEND_URL|g" "/usr/share/nginx/html/assets/config.json"
+sed -i "s|localhost|$SOCKET_URL|g" "/usr/share/nginx/html/assets/config.json"
 
 echo "cat config.json"
 cat /usr/share/nginx/html/assets/config.json
-
-/etc/init.d/nginx reload
 
 pm2 start /app/backend/dist/src/index.js && nginx -g 'daemon off;'
