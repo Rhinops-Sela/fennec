7dff8339651f3c8d4ea8fca514876b38df95900f
shaharblanksela <shaharb@sela.co.il>
2021-10-18 16:23:59 +0300

    change url fix

--- devops/metadata/script.sh
+++ devops/metadata/script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sed -i "s|localhost|$BACKEND_URL|g" "/usr/share/nginx/html/assets/config.json"
-sed -i "s|localhost|$SOCKET_URL|g" "/usr/share/nginx/html/assets/config.json"
 
 echo "cat config.json"
 cat /usr/share/nginx/html/assets/config.json
