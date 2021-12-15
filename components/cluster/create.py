ee4d668631859b4e0df1bdfaa24bba64cb28870a
Ilia <iliag@sela.co.il>
2021-12-15 07:15:10 +0200

    install cluster components only when installing new

--- components/cluster/create.py
+++ components/cluster/create.py
@@ -21,66 +21,66 @@ if not cluster.check_if_cluster_exists() or not allow_skip:
     cluster.install_folder(folder='05.cert-manager/kubectl',
                            namespace="cert-manager")
 
-# Install HPA
-install_HPA = cluster.execution.get_local_parameter('INSTALL_CLUSTER_HPA')
-if install_HPA:
-    hpa_instsllation = os.path.join(
-        cluster.execution.templates_folder, "03.hpa", "hpa.yaml")
-    cluster.create_namespace("horizontal-pod-scaler")
-    cluster.install_file(hpa_instsllation, "horizontal-pod-scaler")
+    # Install HPA
+    install_HPA = cluster.execution.get_local_parameter('INSTALL_CLUSTER_HPA')
+    if install_HPA:
+        hpa_instsllation = os.path.join(
+            cluster.execution.templates_folder, "03.hpa", "hpa.yaml")
+        cluster.create_namespace("horizontal-pod-scaler")
+        cluster.install_file(hpa_instsllation, "horizontal-pod-scaler")
 
-# Install Cluster auto scaler
-install_cluster_autoscaler = cluster.execution.get_local_parameter(
-    'INSTALL_CLUSTER_AUTOSCALER')
-if install_cluster_autoscaler:
-    cluster_auto_scaler_chart = Helm(
-        os.path.dirname(__file__), "cluster-autoscaler")
-    values_file_path = os.path.join(
-        cluster.execution.templates_folder, "04.cluster-autoscaler", "auto_scaler.yaml")
-    cluster_auto_scaler_chart.install_chart(release_name="stable", chart_url="https://charts.helm.sh/stable",
-                                            additional_values=[
-                                                f"--values {values_file_path}",
-                                                f"--set autoDiscovery.clusterName={cluster.execution.cluster_name}",
-                                                f"--set awsRegion={cluster.execution.cluster_region}",
-                                                "--version 7.0.0"
-                                            ])
-    # Install Nginx Controller
-    install_ingress_controller = cluster.execution.get_local_parameter(
-        'INSTALL_INGRESS_CONTROLER')
-    if install_ingress_controller:
-        cluster.install_folder(folder="06.nginx")
+    # Install Cluster auto scaler
+    install_cluster_autoscaler = cluster.execution.get_local_parameter(
+        'INSTALL_CLUSTER_AUTOSCALER')
+    if install_cluster_autoscaler:
+        cluster_auto_scaler_chart = Helm(
+            os.path.dirname(__file__), "cluster-autoscaler")
+        values_file_path = os.path.join(
+            cluster.execution.templates_folder, "04.cluster-autoscaler", "auto_scaler.yaml")
+        cluster_auto_scaler_chart.install_chart(release_name="stable", chart_url="https://charts.helm.sh/stable",
+                                                additional_values=[
+                                                    f"--values {values_file_path}",
+                                                    f"--set autoDiscovery.clusterName={cluster.execution.cluster_name}",
+                                                    f"--set awsRegion={cluster.execution.cluster_region}",
+                                                    "--version 7.0.0"
+                                                ])
+        # Install Nginx Controller
+        install_ingress_controller = cluster.execution.get_local_parameter(
+            'INSTALL_INGRESS_CONTROLER')
+        if install_ingress_controller:
+            cluster.install_folder(folder="06.nginx")
 
-    # Install Cluster dashboard
-    install_cluster_dashboard = cluster.execution.get_local_parameter(
-    'INSTALL_CLUSTER_DASHBOARD')
-if install_cluster_dashboard:
-    values_file_path = os.path.join(
-        cluster.execution.templates_folder, "07.dashboard", "ingress.yaml")
-    user_url = cluster.execution.get_local_parameter(
-        'CLUSTER_DASHBOARD_URL')
-    values_to_replace = {'CLUSTER_DASHBOARD_URL': f'{user_url}'}
-    values_file_path_execution = Helper.replace_in_file(
-        values_file_path, values_to_replace, 100)
-    cluster.install_folder(folder="07.dashboard", namespace="kubernetes-dashboard")
-    cluster.export_secret(secret_name="admin-user",
-                            namespace="kube-system",
-                            output_file_name="dashboard",
-                            decode=True)
-    ingress_address = cluster.get_ingress_address(
-        'kubernetes-dashboard-ingress', 'kubernetes-dashboard')
-    core_dns = CoreDNS(os.path.dirname(__file__))
-    core_dns.add_records(f"{user_url}={ingress_address}")
+        # Install Cluster dashboard
+        install_cluster_dashboard = cluster.execution.get_local_parameter(
+        'INSTALL_CLUSTER_DASHBOARD')
+    if install_cluster_dashboard:
+        values_file_path = os.path.join(
+            cluster.execution.templates_folder, "07.dashboard", "ingress.yaml")
+        user_url = cluster.execution.get_local_parameter(
+            'CLUSTER_DASHBOARD_URL')
+        values_to_replace = {'CLUSTER_DASHBOARD_URL': f'{user_url}'}
+        values_file_path_execution = Helper.replace_in_file(
+            values_file_path, values_to_replace, 100)
+        cluster.install_folder(folder="07.dashboard", namespace="kubernetes-dashboard")
+        cluster.export_secret(secret_name="admin-user",
+                                namespace="kube-system",
+                                output_file_name="dashboard",
+                                decode=True)
+        ingress_address = cluster.get_ingress_address(
+            'kubernetes-dashboard-ingress', 'kubernetes-dashboard')
+        core_dns = CoreDNS(os.path.dirname(__file__))
+        core_dns.add_records(f"{user_url}={ingress_address}")
 
-    # Install External DNS
-install_cluster_external_dns = cluster.execution.get_local_parameter('ROUTE_53_ZONE_ID')
-if install_cluster_external_dns:
-    Helper.print_log("Will deploy external DNS component")
-    deployment_file = os.path.join(
-        cluster.execution.templates_folder, "09.external_dns", "deployment.yaml")
-    deployment_file_execution = Helper.replace_in_file(deployment_file, {
-        'CLUSTER_NAME': f'{cluster.execution.cluster_name}'})
-    cluster.install_file(deployment_file_execution, namespace='external-dns')
-install_vpn = cluster.execution.get_local_parameter('INSTALL_VPN')
+        # Install External DNS
+    install_cluster_external_dns = cluster.execution.get_local_parameter('ROUTE_53_ZONE_ID')
+    if install_cluster_external_dns:
+        Helper.print_log("Will deploy external DNS component")
+        deployment_file = os.path.join(
+            cluster.execution.templates_folder, "09.external_dns", "deployment.yaml")
+        deployment_file_execution = Helper.replace_in_file(deployment_file, {
+            'CLUSTER_NAME': f'{cluster.execution.cluster_name}'})
+        cluster.install_file(deployment_file_execution, namespace='external-dns')
+    install_vpn = cluster.execution.get_local_parameter('INSTALL_VPN')
 if install_vpn:
     template_path = os.path.join(
             cluster.execution.templates_folder, "08.openvpn", "vpn-ng-template.json")
